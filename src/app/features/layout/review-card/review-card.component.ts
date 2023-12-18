484ae9a3a5dd9b041d9ef67b07dece258467d2e5
khanh.cao <khanh.cao@impartner.com>
2023-12-18 17:06:40 +0700

    1218 - update review card, customers slide

--- src/app/features/layout/review-card/review-card.component.ts
+++ src/app/features/layout/review-card/review-card.component.ts
@@ -1,47 +1,58 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-review-card',
   templateUrl: './review-card.component.html',
-  styleUrls: ['./review-card.component.scss']
+  styleUrls: ['./review-card.component.scss'],
 })
 export class ReviewCardComponent implements OnInit {
   @ViewChild('carousel') carousel!: ElementRef;
   currentIndex = 0;
   items = [
     {
-      image: 'https://images.unsplash.com/photo-1572561300743-2dd367ed0c9a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300',
+      image: '',
       name: 'Ronne Galle',
-      position: 'Project Manager',
-      comment: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat'
+      position: 'CEO',
+      comment: `Công ty cung cấp các dịch vụ thuế chất lượng cao, giá cả hợp lý và 
+      thời gian hoàn thành nhanh chóng. Tôi rất hài lòng với dịch vụ.`,
     },
     {
-      image: 'https://images.unsplash.com/photo-1572561300743-2dd367ed0c9a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300',
+      image: '',
       name: 'Ronne Galle 2',
-      position: 'Project Manager',
-      comment: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat'
+      position: 'CEO',
+      comment: `Dịch vụ chuyên nghiệp và chất lượng. Đội ngũ tư vấn nhiệt tình, hiểu rõ về nhu cầu của khách hàng. 
+        Chúng tôi đã có những trải nghiệm cực kỳ hài lòng với dịch vụ của công ty.`,
     },
     {
-      image: 'https://images.unsplash.com/photo-1572561300743-2dd367ed0c9a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300',
+      image: '',
       name: 'Ronne Galle 3',
-      position: 'Project Manager',
-      comment: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat'
+      position: 'Founder',
+      comment: `Công ty cung cấp dịch vụ chất lượng và đẳng cấp. Chúng tôi đã tìm thấy sự chuyên nghiệp và 
+        kiến thức sâu rộng từ đội ngũ nhân viên. Đây là đối tác lý tưởng cho khách hàng.`,
     },
     {
-      image: 'https://images.unsplash.com/photo-1572561300743-2dd367ed0c9a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300',
+      image: '',
       name: 'Ronne Galle 4',
-      position: 'Project Manager',
-      comment: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat'
+      position: 'CEO',
+      comment: `Tôi đã hợp tác với công ty trong nhiều dự án và luôn ấn tượng bởi sự chuyên nghiệp và tận tâm của họ. 
+      Chất lượng nội dung luôn đáp ứng cao nhất, và đội ngũ nhân viên có kiến thức sâu rộng về lĩnh vực thuế.`,
     },
     {
-      image: 'https://images.unsplash.com/photo-1572561300743-2dd367ed0c9a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300',
+      image: '',
       name: 'Ronne Galle 5',
-      position: 'Project Manager',
-      comment: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat'
-    }
-  ]
+      position: 'CEO',
+      comment: `Công ty mang lại giá trị chất lượng và sự uy tín. Đội ngũ nhân viên có khả năng hiểu rõ về nhu cầu và 
+        đưa ra tối ưu cho khách hàng. Tôi tin tưởng và rất hài lòng với kết quả mỗi lần hợp tác.`,
+    },
+  ];
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this.initCarousel();
