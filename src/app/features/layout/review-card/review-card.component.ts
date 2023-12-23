e0399e094d69febbabfb6c164cc974b56b49266c
CAOKHANH <CAO KHANH@DESKTOP-CKG9NKJ>
2023-12-23 15:59:06 +0700

    1223 - update image review card

--- src/app/features/layout/review-card/review-card.component.ts
+++ src/app/features/layout/review-card/review-card.component.ts
@@ -8,42 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class ReviewCardComponent implements OnInit {
   items = [
     {
-      image:
-        'https://images.unsplash.com/photo-1572561300743-2dd367ed0c9a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300',
+      image: 'https://i.imgur.com/wGoNqk3.jpg',
       name: 'Đặng Văn Hùng',
-      position: 'CEO công ty TNHH NetTeck',
+      position: 'CEO công ty HPTeck',
       comment: `Công ty cung cấp các dịch vụ thuế chất lượng cao, giá cả hợp lý và 
       thời gian hoàn thành nhanh chóng. Tôi rất hài lòng với dịch vụ.`,
     },
     {
-      image:
-        'https://images.unsplash.com/photo-1588361035994-295e21daa761?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=301&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=301',
+      image: 'https://i.imgur.com/ChIHpy6.jpg',
       name: 'Hồ Thu Thảo',
-      position: 'CEO công ty TNHH NetTeck',
+      position: 'CEO công ty TNHH Asnet',
       comment: `Dịch vụ chuyên nghiệp và chất lượng. Đội ngũ tư vấn nhiệt tình, hiểu rõ về nhu cầu của khách hàng. 
         Chúng tôi đã có những trải nghiệm cực kỳ hài lòng với dịch vụ của công ty.`,
     },
     {
-      image:
-        'https://images.unsplash.com/photo-1575377222312-dd1a63a51638?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=302&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=302',
-      name: 'Nguyễn Phương Trang',
-      position: 'Founder công ty TNHH NetTeck',
+      image: 'https://i.imgur.com/TA72NnW.jpg',
+      name: 'Nguyễn Hoàng Thông',
+      position: 'Founder công ty GreenDay',
       comment: `Công ty cung cấp dịch vụ chất lượng và đẳng cấp. Chúng tôi đã tìm thấy sự chuyên nghiệp và 
         kiến thức sâu rộng từ đội ngũ nhân viên. Đây là đối tác lý tưởng cho khách hàng.`,
     },
     {
-      image:
-        'https://images.unsplash.com/photo-1549836938-d278c5d46d20?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=303&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=303',
+      image: 'https://i.imgur.com/JNZZ3bA.jpg',
       name: 'Lê Diệu Nhi',
-      position: 'CEO công ty TNHH NetTeck',
+      position: 'CEO công ty TNHH T-Tech',
       comment: `Tôi đã hợp tác với công ty trong nhiều dự án và luôn ấn tượng bởi sự chuyên nghiệp và tận tâm của họ. 
       Chất lượng và đội ngũ nhân viên có kiến thức sâu rộng về lĩnh vực thuế.`,
     },
     {
-      image:
-        'https://images.unsplash.com/photo-1588361035994-295e21daa761?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=301&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=301',
+      image: 'https://i.imgur.com/n2ssjFH.jpg',
       name: 'Trần Hà',
-      position: 'CEO công ty TNHH NetTeck',
+      position: 'CEO công ty TNHH JSC',
       comment: `Công ty mang lại giá trị chất lượng và sự uy tín. Đội ngũ nhân viên có khả năng hiểu rõ về nhu cầu và 
         đưa ra tối ưu cho khách hàng.`,
     },
