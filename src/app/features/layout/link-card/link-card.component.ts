a07709040f5c4a77dda84751563e1c05dbb83826
CAOKHANH <CAO KHANH@DESKTOP-CKG9NKJ>
2023-12-24 15:10:14 +0700

    1224 - add link card

---
+++ src/app/features/layout/link-card/link-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-card',
+  templateUrl: './link-card.component.html',
+  styleUrls: ['./link-card.component.scss'],
+})
+export class LinkCardComponent {}
