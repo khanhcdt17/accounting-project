4731f9419b49d3de8f84a379d7369e6cdaa804d1
khanh.cao <khanh.cao@impartner.com>
2024-01-11 08:07:40 +0700

    0111 update service

--- src/app/features/service-page/service-page.component.ts
+++ src/app/features/service-page/service-page.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-service-page',
   templateUrl: './service-page.component.html',
-  styleUrls: ['./service-page.component.scss']
+  styleUrls: ['./service-page.component.scss'],
 })
 export class ServicePageComponent {
-
+  services = [
+    { img: '', label: 'Kế toán' },
+    { img: '', label: 'Đại lý thuế' },
+    { img: '', label: 'Quyết toán thuế' },
+    { img: '', label: 'Đăng ký kinh doanh' },
+    { img: '', label: 'Bảo hiểm xã hội' },
+    { img: '', label: 'Đào tạo kế toán' },
+  ];
 }
