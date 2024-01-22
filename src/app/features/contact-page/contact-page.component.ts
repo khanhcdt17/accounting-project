accf8595c81218dadaa6ab4bb5ecf2e00cf8e50a
CAOKHANH <CAO KHANH@DESKTOP-CKG9NKJ>
2024-01-22 22:57:06 +0700

    0122 deploy service done

--- src/app/features/contact-page/contact-page.component.ts
+++ src/app/features/contact-page/contact-page.component.ts
@@ -1,59 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ToastComponent } from '../common/toast/toast.component';
-import { ContactApiService } from '../api/contact-api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss'],
 })
-export class ContactPageComponent implements OnInit {
-  contactForm!: FormGroup;
-  lastSubmitTime!: number;
-  cooldownTime: number = 5000;
-
-  constructor(private fb: FormBuilder, private contactApi: ContactApiService) {}
-  @ViewChild(ToastComponent) toast!: ToastComponent;
-  ngOnInit() {
-    this.lastSubmitTime = 0;
-    this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      phone: ['', Validators.required],
-      email: ['', Validators.email],
-      gender: [''],
-      description: [''],
-    });
-  }
-
-  onSubmit() {
-    const currentTime = Date.now();
-    this.contactForm.markAllAsTouched();
-    if (this.contactForm.valid) {
-      if (currentTime - this.lastSubmitTime >= this.cooldownTime) {
-        const formData = {
-          ['Thông báo']: 'Người dùng đã gửi thông tin tư vấn',
-          ['Danh xưng']: this.contactForm.controls['gender']?.value
-            ? 'Anh'
-            : 'Chị',
-          ['Tên']: this.contactForm.controls['name']?.value,
-          ['Số điện thoại']: this.contactForm.controls['phone']?.value,
-          ['Địa chỉ email']: this.contactForm.controls['email']?.value,
-          ['Thông tin cần tư vấn']:
-            this.contactForm.controls['description']?.value,
-        };
-
-        this.contactApi.submitForm(formData).subscribe((response) => {
-          this.toast.open('Gửi thông tin thành công', 'alert-success');
-        });
-        this.contactForm.reset();
-      } else {
-        this.toast.open(
-          'Vui lòng gửi lại thông tin sau 5 giây',
-          'alert-warning'
-        );
-      }
-      this.lastSubmitTime = currentTime;
-    }
-  }
+export class ContactPageComponent {
+  constructor() {}
 }
