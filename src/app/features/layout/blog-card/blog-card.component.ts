7d7e7f3364ebd9dd7c14d6cfcf70a7c6b0582e9d
CAOKHANH <CAO KHANH@DESKTOP-CKG9NKJ>
2024-01-27 19:00:20 +0700

    deploy homepage mobile mode

--- src/app/features/layout/blog-card/blog-card.component.ts
+++ src/app/features/layout/blog-card/blog-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonStore } from '../../common/common.store';
 import { DEFAULT_URL, TAB_CONTENT } from '../../common/constant.model';
 
@@ -10,10 +10,19 @@ import { DEFAULT_URL, TAB_CONTENT } from '../../common/constant.model';
 export class BlogCardComponent {
   defaultUrl = DEFAULT_URL;
   constructor(private commonStore: CommonStore) {}
-
+  chuck = 1;
+  itemsPerSlide = 1;
   vm$ = this.commonStore.select((state) => {
     return {
       displayContents: state.displayContents,
     };
   });
+  OnInit() {
+    if (window.innerWidth <= 1550) {
+      this.itemsPerSlide = 2;
+    } else if (window.innerWidth <= 1120) {
+      this.itemsPerSlide = 1;
+      this.chuck = 1;
+    }
+  }
 }
