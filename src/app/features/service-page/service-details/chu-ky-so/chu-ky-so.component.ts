e6d05c8cf28635f5d393ebeab31d5589005e034e
CAOKHANH <CAO KHANH@DESKTOP-CKG9NKJ>
2024-10-27 18:25:50 +0700

    add dich vu khac

---
+++ src/app/features/service-page/service-details/chu-ky-so/chu-ky-so.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chu-ky-so',
+  templateUrl: './chu-ky-so.component.html',
+  styleUrls: ['./chu-ky-so.component.scss']
+})
+export class ChuKySoComponent {
+
+}
---
+++ src/app/features/service-page/service-details/giai-the-doanh-nghiep/giai-the-doanh-nghiep.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-giai-the-doanh-nghiep',
+  templateUrl: './giai-the-doanh-nghiep.component.html',
+  styleUrls: ['./giai-the-doanh-nghiep.component.scss']
+})
+export class GiaiTheDoanhNghiepComponent {
+
+}
